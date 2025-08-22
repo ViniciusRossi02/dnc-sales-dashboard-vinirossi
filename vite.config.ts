4d71ec4b722c6db3d3c9f49703ea8b9f8349281d
unknown <rossivini1010@gmail.com>
2025-08-22 17:53:58 -0300

    Feat material-ui-insert

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,12 @@ import path from 'node:path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps:{
+    include: ['@emotion/styled']
+  },
   resolve:{
     alias:{
-      '@': path.resolve(__dirname, '/src')
+      '@': path.resolve(__dirname, './src')
     }
   }
 })
