b74df51b13048a28a73eadd31c88eca4f5bbc5d4
unknown <rossivini1010@gmail.com>
2025-08-26 20:59:44 -0300

    Feat: Header Component

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,12 @@ import { StrictMode } from 'react'
 import { ThemeProvider } from 'styled-components'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-import { GlobalStyle, darkTheme } from './styles/'
+import { GlobalStyle, lightTheme } from './styles/'
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ThemeProvider theme={darkTheme}>
+    <ThemeProvider theme={lightTheme}>
       <GlobalStyle/>
       <App />
     </ThemeProvider>
