b20ef4037431193508e898f0a9d6c2b3b7cac3d8
unknown <rossivini1010@gmail.com>
2025-09-22 16:48:32 -0300

    Feat: Create Profile Flow

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -23,8 +23,13 @@ import { jwtExpirationDateConverter, pxToRem } from '@/utils'
 //TYPES
 import { type DecodedJWT ,type MessageProps, type LoginData, type LoginPostData } from '@/types'
 
+//REDUX
+import { useSelector } from 'react-redux'
+import type { RootState } from '@/redux'
+
 function Login() {
   const navigate = useNavigate()
+  const {email, message} = useSelector((state: RootState) => state.createprofile)
   const inputs = [
     { type: 'email', placeholder: 'Email' },
     { type: 'password', placeholder: 'Senha' },
@@ -36,7 +41,7 @@ function Login() {
   const { formValues, formValid, handleChange } = useFormValidation(inputs)
 
   const handleMessage = (): MessageProps => {
-    if (!error) return { msg: '', type: 'success' }
+    if (!error) return { msg: message ?? '', type: 'success' }
     switch (error) {
       case 401:
         return {
@@ -69,6 +74,13 @@ function Login() {
     }
     if(Cookies.get('Authorization')) navigate('/home')
   }, [data, navigate])
+
+  useEffect(()=>{
+    if (email) {
+      handleChange(0, email)
+    }
+  }, [email])
+
 
   return (
     <>
